ec9f07930d029245d98007483d3382a69e06b622
Attila Gal <attila.gal@c4j.be>
2016-12-11 10:21:32 +0100

    removed file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -51,11 +51,6 @@ module.exports = {
         test: /\.html$/,
         exclude: /node_modules/,
         loader: 'raw-loader'
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
-        exclude: /node_modules/,
-        loader: 'file'
       }
     ]
   },
