a16b8c9f38c2b70393777d64024e2d1b0c2d6136
Attila Gal <attila.gal@c4j.be>
2016-12-11 10:22:12 +0100

    removed @NgModule declation from ng1 module

--- src/menu/index.ts
+++ src/menu/index.ts
@@ -1,6 +1,5 @@
 // This module is purely written in Angular 1.
 import * as angular from 'angular';
-import {NgModule} from '@angular/core';
 import {MenuCmp} from './menu_cmp';
 
 export const MenuModule = angular.module('MenuModule', ['ngRoute']);
@@ -8,8 +7,3 @@ MenuModule.component('menu', MenuCmp);
 MenuModule.config(($routeProvider) => {
   $routeProvider.when('/', {template : '<menu></menu>'});
 });
-
-// techncially, this is not required, but it is nice to have it for consistency
-@NgModule({})
-export class MenuNgModule {
-}
